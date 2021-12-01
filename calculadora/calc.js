c650cc14dc89df4ddcc3d9fd608e741d827e5dd7
Miguel Labrador <github4138@mlsdm.com>
2021-12-01 16:40:27 +0100

    Finalizada función insertar(), ya mete números

--- calculadora/calc.js
+++ calculadora/calc.js
@@ -3,12 +3,25 @@ function insertar() {
     var valor = event.target.innerText;
     var pantalla = document.getElementById("resultado");
 
-    if (valor === ",") {
-        var inicial = 0;
-        var decimal = 0;
-        pantalla.innerText = ",";
-    } else {
+    // Añadimos decimales, comprueba que si ya hay una coma no añada otra
+    if (valor === "," && comprobarComa() === true) {
+
+    } // Si empieza por cero, hay una coma y el valor es distinto a coma que lo añada después
+    else if (comprobarCero() === "0" && valor !== "," && comprobarComa === true) {
+        pantalla.innerText = pantalla.innerText + valor;
+
+    } // Si hay solo un cero y metemos una coma que la ponga después del cero
+    else if (comprobarCero() === "0" && valor === ",") {
+        pantalla.innerText = pantalla.innerText + ",";
+
+    } // Si empieza por cero, no hay coma y el valor es distinto a coma que lo sustituya
+    else if (comprobarCero() === "0" && valor !== "," && comprobarComa() === false) {
         pantalla.innerText = valor;
+
+    } // Para todo lo demás, añadir el valor pulsado al mostrado en pantalla 
+    else {
+        pantalla.innerText = pantalla.innerText + valor;
+
     }
 
 }
@@ -32,7 +45,7 @@ function sumar() {
 }
 
 function comprobarComa() {
-    
+
     var pantalla = String(document.getElementById("resultado").innerText);
 
     return pantalla.includes(",");
